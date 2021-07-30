df8c6720969c2944aa28e0b2d7f6c46bb14066e9
Ne0nd0g <Russel.VanTuyl@gmail.com>
2021-07-30 14:31:35 -0400

    Gandalf - https://github.com/r00t0v3rr1d3/merlin/commit/aa7b4548385cb65dda999dc1a7ec05750e521942

--- commands/native.go
+++ commands/native.go
@@ -21,6 +21,7 @@ import (
 	// Standard
 	"fmt"
 	"io/ioutil"
+	"math"
 	"net"
 	"os"
 	"path/filepath"
@@ -80,6 +81,8 @@ func Native(cmd jobs.Command) jobs.Results {
 		} else {
 			results.Stdout = fmt.Sprintf("Current working directory: %s", dir)
 		}
+	case "sdelete":
+		results.Stdout, results.Stderr = sdelete(cmd.Args[1])
 	default:
 		results.Stderr = fmt.Sprintf("%s is not a valid NativeCMD type", cmd.Command)
 	}
@@ -172,4 +175,72 @@ func killProcess(pid string) (stdout string, stderr string) {
 
 	stdout = fmt.Sprintf("Successfully killed pid %d", targetpid)
 	return
+}
+
+// sdelete securely deletes a file
+func sdelete(targetfile string) (string, string) {
+	var targetFile = targetfile
+	var resp string
+	var stderr string
+
+	// make sure we open the file with correct permission
+	// otherwise we will get the bad file descriptor error
+	file, err := os.OpenFile(targetFile, os.O_RDWR, 0666)
+
+	if err != nil {
+		stderr = fmt.Sprintf("Error opening file: %s\r\n%s", targetfile, err.Error())
+		return resp, stderr
+	}
+
+	// find out how large is the target file
+	fileInfo, err := file.Stat()
+
+	if err != nil {
+		stderr = fmt.Sprintf("Error determining file size: %s\r\n%s", targetfile, err.Error())
+		return resp, stderr
+	} else {
+
+		// calculate the new slice size
+		// based on how large our target file is
+		var fileSize int64 = fileInfo.Size()
+		const fileChunk = 1 * (1 << 20) //1MB Chunks
+
+		// calculate total number of parts the file will be chunked into
+		totalPartsNum := uint64(math.Ceil(float64(fileSize) / float64(fileChunk)))
+
+		lastPosition := 0
+
+		for i := uint64(0); i < totalPartsNum; i++ {
+			partSize := int(math.Min(fileChunk, float64(fileSize-int64(i*fileChunk))))
+			partZeroBytes := make([]byte, partSize)
+
+			// fill out the part with zero value
+			copy(partZeroBytes[:], "0")
+
+			// overwrite every byte in the chunk with 0
+			n, err := file.WriteAt([]byte(partZeroBytes), int64(lastPosition))
+
+			if err != nil {
+				stderr = fmt.Sprintf("Error over writing file: %s\r\n%s", targetfile, err.Error())
+				return resp, stderr
+			}
+
+			resp += fmt.Sprintf("Wiped %v bytes.\n", n)
+
+			// update last written position
+			lastPosition = lastPosition + partSize
+		}
+
+		file.Close()
+
+		// finally, remove/delete our file
+		err = os.Remove(targetFile)
+		if err != nil {
+			stderr = fmt.Sprintf("Error deleting file: %s\r\n%s", targetfile, err.Error())
+			return resp, stderr
+		}
+		resp += fmt.Sprintf("Securely deleted file: %s\n", targetfile)
+
+		return resp, stderr
+	}
 }
